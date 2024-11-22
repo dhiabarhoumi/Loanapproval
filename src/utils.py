b64d6271babda01a6941ee5e8d7973432a7122c5
Dhieddine Barhoumi <dhiabarhoumi2608@gmail.com>
2024-11-22 09:37:02 +0100

    first chnages to model trainer and utils

--- src/utils.py
+++ src/utils.py
@@ -33,24 +33,24 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             model = list(models.values())[i]
             para = param[model_name]
 
-            # Perform GridSearchCV for hyperparameter tuning
-            gs = GridSearchCV(model, para, cv=3, scoring='accuracy')  # You can change 'accuracy' to other scoring metrics
+           
+            gs = GridSearchCV(model, para, cv=3, scoring='accuracy')  
             gs.fit(X_train, y_train)
 
-            # Set the best parameters to the model
+            
             model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            # Make predictions
+           
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            # Evaluate model performance
+           
             train_accuracy = accuracy_score(y_train, y_train_pred)
             test_accuracy = accuracy_score(y_test, y_test_pred)
             test_f1_score = f1_score(y_test, y_test_pred)  
             
-            # Store metrics
+            
             report[model_name] = {
                 'Train Accuracy': train_accuracy,
                 'Test Accuracy': test_accuracy,
